46f6a6dd1183d1bfe3e7186cb90e60e0a8f2cac7
smith <smith@futureworkz.com>
2016-07-02 12:13:06 +0700

    Update comments_controller#create, #reply and spec

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,41 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :build_comment, only: [:create, :reply]
   
   def index
     @comment    = Comment.new
-    @comments   = Comment.all
+    @comments   = Comment.where(parent_id: nil)
+  end
+
+  def create
+    if @comment.save
+      redirect_to comments_path, notice: "You have successfuly created."
+    else
+      redirect_to comments_path, notice: "There is an error when create your comment."
+    end
+  end
+
+  def reply
+    if @comment.save
+      redirect_to comments_path, notice: "You have successfuly created."
+    else
+      redirect_to comments_path, notice: "There is an error when create your comment."
+    end
+  end
+
+  protected
+
+  def comment_id
+    params[:id]
+  end
+
+  def comment_params
+    data              = params.require(:comment).permit(:message)
+    data[:parent_id]  = comment_id if comment_id.present?
+    data
+  end
+
+  def build_comment
+    @comment = current_user.comments.new(comment_params)
   end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -16,4 +16,55 @@ RSpec.describe CommentsController, type: :controller do
       expect(assigns(:comment)).to be_a Comment
     end
   end
+
+  describe '#create' do
+    def do_request
+      post :create, comment: comment.attributes
+    end
+
+    context 'Success' do 
+      let(:comment) { build(:comment, user: user, id: '') }
+
+      it 'save a comment' do
+        expect{ do_request }.to change(Comment, :count).by(1)
+      end
+    end
+
+    context 'failure' do
+      let(:comment)      { build(:comment, message: '', id: '') }
+
+      it 'renders new on failure' do
+        expect{ do_request }.to_not change{ Comment.count }
+      end
+    end
+  end
+
+  describe '#reply' do
+    def do_request
+      post :reply, comment: reply.attributes, id: comment.id
+    end
+
+    let(:comment)       { create(:comment, user: user) }
+    let(:user_james)    { create(:user, name: "James") }
+
+    context 'Success' do 
+      let(:reply)     { build(:comment, user_id: user_james) }
+
+      it 'save a comment' do
+        do_request
+
+        expect(Comment.all.size).to eq 2
+      end
+    end
+
+    context 'failure' do
+      let(:reply)      { build(:comment, message: '', user_id: user_james) }
+
+      it 'renders new on failure' do
+        do_request
+
+        expect(Comment.all.size).to eq 1
+      end
+    end
+  end
 end

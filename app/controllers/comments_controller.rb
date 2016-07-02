c09d664a4b543594ec710385bf05c6f47115c325
smith <smith@futureworkz.com>
2016-07-02 14:12:21 +0700

    Change the function create new comment and spec

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :build_comment, only: [:create, :reply]
   
   def index
     @comment    = Comment.new
@@ -8,34 +7,12 @@ class CommentsController < ApplicationController
   end
 
   def create
-    if @comment.save
-      redirect_to comments_path, notice: "You have successfuly created."
-    else
-      redirect_to comments_path, notice: "There is an error when create your comment."
-    end
-  end
-
-  def reply
-    if @comment.save
-      redirect_to comments_path, notice: "You have successfuly created."
-    else
-      redirect_to comments_path, notice: "There is an error when create your comment."
-    end
+    @comment  = current_user.comments.new(comment_params)
+    @status   = @comment.save
   end
 
   protected
-
-  def comment_id
-    params[:id]
-  end
-
   def comment_params
-    data              = params.require(:comment).permit(:message)
-    data[:parent_id]  = comment_id if comment_id.present?
-    data
-  end
-
-  def build_comment
-    @comment = current_user.comments.new(comment_params)
+    params.require(:comment).permit(:message, :parent_id)
   end
 end

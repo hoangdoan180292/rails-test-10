d874036ac6f83d6b5190587f1ddff06e2a0c66f9
smith <smith@futureworkz.com>
2016-07-02 10:33:12 +0700

    Update user model and spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :comments
+
   validates :name,        presence: true
 
   has_attached_file :avatar,
